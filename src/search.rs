ac5ada48d1568c7bd09674cf2bcfbbc7c50f87b3
Jorge Filipe <jfms777@gmail.com>
2026-04-29 19:32:08 -0300

    reduz probes rapidos do ivf

--- src/search.rs
+++ src/search.rs
@@ -3,7 +3,7 @@ use crate::dataset::Dataset;
 #[cfg(target_arch = "x86_64")]
 use std::arch::x86_64::*;
 
-const FAST_NPROBE: usize = 16;
+const FAST_NPROBE: usize = 12;
 const FULL_NPROBE: usize = 24;
 const MAX_CENTROIDS: usize = 4096;
 const VECTOR_SCALE: f32 = 0.0001;
